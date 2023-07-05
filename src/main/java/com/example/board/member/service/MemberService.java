6a99204afaf0711e73d8995dcebe27d6896e9d6d
deerKBS <deer980119@gmail.com>
2023-07-06 00:45:56 +0900

    refactor: MemberId 중복 조회 로직 수정 - exists 메서드 사용하도록 수정

--- src/main/java/com/example/board/member/service/MemberService.java
+++ src/main/java/com/example/board/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.board.member.service;
 
 import com.example.board.config.BaseException;
+import com.example.board.member.dto.SignupRequest;
 import com.example.board.member.repository.MemberRepository;
 import com.example.board.member.repository.entity.Member;
 import com.example.board.util.ApiError;
@@ -15,21 +16,24 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public void register(Member member){
-        Optional<Member> members = memberRepository.findByMemberId(member.getMemberId()); // 중복검사
-        if(members.isPresent()){
-            throw new BaseException(new ApiError("존재하는 id입니다.", 1000));
+    public void register(SignupRequest signupRequest){
+        Member member = signupRequest.toEntity();
+        boolean isExist = memberRepository.existsByMemberId(member.getMemberId()); // 중복검사
+        if(isExist){
+            throw new BaseException(new ApiError("존재하는 id 입니다.", 1000));
         }
 
-        Member member1 = memberRepository.save(member); // 저장
-        if(member.getMemberId().equals(member1.getMemberId())){ // 한번 더 확인
+        if(member.getMemberId().equals(memberRepository.save(member).getMemberId())){ // 저장 & 한번 더 확인
             return;
         }
         throw new BaseException(new ApiError("회원가입 과정에서 문제가 발생하였습니다.", 1001));
     }
 
     public void duplicateCheck(String memberId){
-        Optional<Member> members = memberRepository.findByMemberId(memberId);
-        members.orElseThrow(()->new BaseException(new ApiError("존재하는 id 입니다.",1008)));
+        boolean isExist = memberRepository.existsByMemberId(memberId);
+
+        if(isExist){
+            throw new BaseException(new ApiError("존재하는 id 입니다.",1008));
+        }
     }
 }
